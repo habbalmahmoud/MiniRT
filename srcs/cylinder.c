e01d8f3145fb9f091a39057462e7c5b526b5fb28
habbalmahmoud <mahhabbal.off1@gmail.com>
2025-03-29 17:55:55 +0200

    Norm

--- srcs/cylinder.c
+++ srcs/cylinder.c
@@ -1,53 +1,63 @@
 #include "../includes/miniRT.h"
 
+void int_cyl(t_ray ray, t_cy *cylinder, t_cy_utils *cy_utils)
+{
+	normalize(cylinder->vector, cy_utils->v);
+    cy_utils->r = cylinder->cy_diameter * 0.5f;
+    cy_utils->h = cylinder->cy_height;
+    cy_utils->half_h = cy_utils->h * 0.5f;
+    subtract(ray.orig, cylinder->cor, cy_utils->delta);
+    cy_utils->D_dot_v = dot(ray.dir, cy_utils->v);
+    cy_utils->delta_dot_v = dot(cy_utils->delta, cy_utils->v);
+    scale(cy_utils->v, cy_utils->D_dot_v, cy_utils->temp);
+    subtract(ray.dir, cy_utils->temp, cy_utils->D_perp);
+    scale(cy_utils->v, cy_utils->delta_dot_v, cy_utils->temp);
+    subtract(cy_utils->delta, cy_utils->temp, cy_utils->delta_perp);
+    cy_utils->A = dot(cy_utils->D_perp, cy_utils->D_perp);
+    cy_utils->B = 2.0f * dot(cy_utils->D_perp, cy_utils->delta_perp);
+    cy_utils->C_val = dot(cy_utils->delta_perp, cy_utils->delta_perp) - cy_utils->r*cy_utils->r;
+}
+
+void int_cyl2(t_ray ray, t_cy *cylinder, t_cy_utils *cy_utils)
+{
+	cy_utils->t2 = (-cy_utils->B + cy_utils->sqrt_disc) / (2*cy_utils->A);
+    cy_utils->t_lateral = 1e30;
+    if (cy_utils->t1 > 0.001f)
+	{
+        scale(ray.dir, cy_utils->t1, cy_utils->tempP);
+        add(ray.orig, cy_utils->tempP, cy_utils->P);
+        subtract(cy_utils->P, cylinder->cor, cy_utils->temp_vec);
+        cy_utils->proj = dot(cy_utils->temp_vec, cy_utils->v);
+        if (fabs(cy_utils->proj) <= cy_utils->half_h && cy_utils->t1 < cy_utils->t_lateral)
+            cy_utils->t_lateral = cy_utils->t1;
+    }
+    if (cy_utils->t2 > 0.001f)
+	{
+		scale(ray.dir, cy_utils->t2, cy_utils->tempP);
+		add(ray.orig, cy_utils->tempP, cy_utils->P);
+		subtract(cy_utils->P, cylinder->cor, cy_utils->temp_vec);
+		cy_utils->proj = dot(cy_utils->temp_vec, cy_utils->v);
+		if (fabs(cy_utils->proj) <= cy_utils->half_h && cy_utils->t2 < cy_utils->t_lateral)
+			cy_utils->t_lateral = cy_utils->t2;
+    }
+    if (cy_utils->t_lateral == 1e30)
+        cy_utils->t_lateral = -1.0f;
+}
+
 float intersect_cylinder(t_ray ray, t_cy *cylinder, int *hit_part)
 {
 	t_cy_utils cy_utils;
-    normalize(cylinder->vector, cy_utils.v);
-    cy_utils.r = cylinder->cy_diameter * 0.5f;
-    cy_utils.h = cylinder->cy_height;
-    cy_utils.half_h = cy_utils.h * 0.5f;
-    subtract(ray.orig, cylinder->cor, cy_utils.delta);
-
-    // Compute components perpendicular to the cylinder's axis.
-    cy_utils.D_dot_v = dot(ray.dir, cy_utils.v);
-    cy_utils.delta_dot_v = dot(cy_utils.delta, cy_utils.v);
-    scale(cy_utils.v, cy_utils.D_dot_v, cy_utils.temp);
-    subtract(ray.dir, cy_utils.temp, cy_utils.D_perp);
-    scale(cy_utils.v, cy_utils.delta_dot_v, cy_utils.temp);
-    subtract(cy_utils.delta, cy_utils.temp, cy_utils.delta_perp);
-
-    cy_utils.A = dot(cy_utils.D_perp, cy_utils.D_perp);
-    cy_utils.B = 2.0f * dot(cy_utils.D_perp, cy_utils.delta_perp);
-    cy_utils.C_val = dot(cy_utils.delta_perp, cy_utils.delta_perp) - cy_utils.r*cy_utils.r;
+	int_cyl(ray, cylinder, &cy_utils); 
 
     cy_utils.t_lateral = -1.0f;
     if (fabs(cy_utils.A) > 1e-6) {
-        cy_utils.discriminant = cy_utils.B*cy_utils.B - 4*cy_utils.A*cy_utils.C_val;
-        if (cy_utils.discriminant >= 0) {
-            cy_utils.sqrt_disc = sqrt(cy_utils.discriminant);
-            cy_utils.t1 = (-cy_utils.B - cy_utils.sqrt_disc) / (2*cy_utils.A);
-            cy_utils.t2 = (-cy_utils.B + cy_utils.sqrt_disc) / (2*cy_utils.A);
-            cy_utils.t_lateral = 1e30;
-            if (cy_utils.t1 > 0.001f) {
-                scale(ray.dir, cy_utils.t1, cy_utils.tempP);
-                add(ray.orig, cy_utils.tempP, cy_utils.P);
-                subtract(cy_utils.P, cylinder->cor, cy_utils.temp_vec);
-                cy_utils.proj = dot(cy_utils.temp_vec, cy_utils.v);
-                if (fabs(cy_utils.proj) <= cy_utils.half_h && cy_utils.t1 < cy_utils.t_lateral)
-                    cy_utils.t_lateral = cy_utils.t1;
-            }
-            if (cy_utils.t2 > 0.001f) {
-                scale(ray.dir, cy_utils.t2, cy_utils.tempP);
-                add(ray.orig, cy_utils.tempP, cy_utils.P);
-                subtract(cy_utils.P, cylinder->cor, cy_utils.temp_vec);
-                cy_utils.proj = dot(cy_utils.temp_vec, cy_utils.v);
-                if (fabs(cy_utils.proj) <= cy_utils.half_h && cy_utils.t2 < cy_utils.t_lateral)
-                    cy_utils.t_lateral = cy_utils.t2;
-            }
-            if (cy_utils.t_lateral == 1e30)
-                cy_utils.t_lateral = -1.0f;
-        }
+		cy_utils.discriminant = cy_utils.B*cy_utils.B - 4*cy_utils.A*cy_utils.C_val;
+		if (cy_utils.discriminant >= 0)
+		{
+			cy_utils.sqrt_disc = sqrt(cy_utils.discriminant);
+			cy_utils.t1 = (-cy_utils.B - cy_utils.sqrt_disc) / (2*cy_utils.A);
+			int_cyl2(ray, cylinder, &cy_utils);
+		}
     }
 
     // Compute cap intersections.
